365aaea8663be6344df48299797357546de4c9bb
CarolFOliveira <kaarol.ferreira@gmail.com>
2024-10-09 14:32:31 -0400

    feat: implementa tela de opções de perfis para login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { ButtonComponent } from './components/button/button.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
