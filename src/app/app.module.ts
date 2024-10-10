804bbdc8de8af08139ef9d28142f3a14caba2b86
CarolFOliveira <kaarol.ferreira@gmail.com>
2024-10-10 17:33:06 -0400

    feat(button): refatorar estrutura do componente button e implementar router para navegação entre páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule,Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ButtonComponent } from './components/button/button.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { LoginProfComponent } from './pages/login-prof/login-prof.component'
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'loginProfessor', component: LoginProfComponent}
+  
+]
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ButtonComponent
+    ButtonComponent,
+    LoginComponent,
+    LoginProfComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    RouterModule.forRoot(routes)
   ],
+  // exports:[
+  //   RouterModule
+  // ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input} from '@angular/core';
+
+/**
+ * ButtonComponent
+ * Componente dinâmico de botão customizado.
+ * 
+ * @param texto - Texto que será exibido no botão
+ * @param id - Id do botão
+ * @param class - classe aplicada ao botão, que resultará na criação de um elemento <a> ou <button> e estilização diferente no css
+ * @param routerLink - Rota para navegação, ao clicar no botão
+ * @param type - Tipo do botão
+ * 
+ */
+
 
 @Component({
   selector: 'app-button',
@@ -9,5 +22,8 @@ export class ButtonComponent {
   @Input() texto = '';
   @Input() id = '';
   @Input() class ='';
-  @Input() href = '';
+  @Input() routerLink = '';
+  @Input() type = "";
+
+  
 }
