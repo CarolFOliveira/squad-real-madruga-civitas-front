676f3a1cc4ca1cc812fd7f83ac5e60ebc18a66bf
felipel7 <felipelseven@gmail.com>
2024-10-14 16:04:55 -0300

    feat(LoginAdmin): tratar casos de sucesso e erro da api

--- src/app/features/admin/pages/login/login.component.ts
+++ src/app/features/admin/pages/login/login.component.ts
@@ -1,5 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AuthService,
+  LoginCredentials,
+  LoginResponse,
+} from 'src/app/core/auth/auth.service';
 
 @Component({
   selector: 'app-admin-login',
@@ -16,9 +22,42 @@ export class LoginComponent {
     ]),
   });
 
+  constructor(private authService: AuthService) {}
+
   onSubmit($event: SubmitEvent) {
     $event.preventDefault();
+    if (this.loginForm.invalid) return;
 
-    console.log(this.loginForm.value);
+    this.loginForm.markAsPending();
+
+    const credentials = this.loginForm.value as LoginCredentials;
+    this.authService.login(credentials).subscribe({
+      next: (response) => this.handleLoginSuccess(response),
+      error: (error: HttpErrorResponse) => this.handleLoginError(error),
+    });
+  }
+
+  private handleLoginSuccess(response: LoginResponse) {
+    console.log(response);
+  }
+
+  private handleLoginError(error: HttpErrorResponse) {
+    switch (error.status) {
+      case 401:
+        this.loginForm.setErrors({ unauthorized: true });
+        break;
+
+      // Remover esse caso (404) antes de fazer o PR
+      case 404:
+        this.loginForm.setErrors({ unauthorized: true });
+        break;
+
+      case 0:
+        console.log('Sem conexão com a internet.');
+        break;
+
+      default:
+        console.log('Erro inesperado.');
+    }
   }
 }
