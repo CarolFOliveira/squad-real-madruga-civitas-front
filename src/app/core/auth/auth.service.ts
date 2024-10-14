2f216944990b088c9bf2ac3a100a0f217a161e5e
felipel7 <felipelseven@gmail.com>
2024-10-14 16:03:00 -0300

    feat: utilizar servico para conectar com backend

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,7 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token?: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  login(credentials: LoginCredentials) {
+    // TODO: conectar corretamente com o endpoint da API
+    return this.http.post<LoginResponse>('/api/login', credentials);
+  }
 }
--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,10 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
 })
 export class AdminModule {}
