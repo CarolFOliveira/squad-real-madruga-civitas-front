ea7357e38e69618a35c1787472bda3a7bb8a6575
felipel7 <felipelseven@gmail.com>
2024-10-16 15:41:38 -0300

    docs: melhorar documentacao no servico storage

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import {
-  AdminLoginCredentials,
-  AdminLoginResponse,
+  LoginCredentials,
+  LoginResponse,
 } from 'src/app/shared/interfaces/auth.interfaces';
 
 @Injectable({
@@ -11,9 +12,20 @@ import {
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
+  // TODO: mudar essa implementação para promises.
 
-  login(credentials: AdminLoginCredentials) {
+  /**
+   * login
+   *
+   * Autentica um usuário enviando suas credenciais de login para o backend.
+   *
+   * @param credentials - As credenciais de login do usuário
+   * @param credentials.email - O endereço de e-mail do usuário
+   * @param credentials.password - A senha do usuário
+   * @returns Um `Observable` contendo a resposta de login, que inclui um token se a autenticação for bem-sucedida.
+   */
+  public login(credentials: LoginCredentials): Observable<LoginResponse> {
     // TODO: conectar corretamente com o endpoint da API
-    return this.http.post<AdminLoginResponse>('/api/login', credentials);
+    return this.http.post<LoginResponse>('/api/login', credentials);
   }
 }
