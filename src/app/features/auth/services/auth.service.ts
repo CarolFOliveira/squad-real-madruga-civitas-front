782a226667b50f1749b7dc545e28799992727850
felipel7 <felipelseven@gmail.com>
2024-10-16 17:20:29 -0300

    fix(AuthService): corrigir problema de fluxo continuo de dados

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 
 import {
   LoginCredentials,
@@ -12,7 +12,6 @@ import {
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
-  // TODO: mudar essa implementação para promises.
 
   /**
    * login
@@ -23,9 +22,19 @@ export class AuthService {
    * @param credentials.email - O endereço de e-mail do usuário
    * @param credentials.password - A senha do usuário
    * @returns Um `Observable` contendo a resposta de login, que inclui um token se a autenticação for bem-sucedida.
+   *
+   * @example
+   * ```ts
+   * this.authService.login(credentials).subscribe({
+   *    next: (response) => this.handleLoginSuccess(response),
+   *    error: (error: HttpErrorResponse) => this.handleLoginError(error),
+   * });
+   * ```
    */
   public login(credentials: LoginCredentials): Observable<LoginResponse> {
     // TODO: conectar corretamente com o endpoint da API
-    return this.http.post<LoginResponse>('/api/login', credentials);
+    return this.http
+      .post<LoginResponse>('/api/login', credentials)
+      .pipe(take(1));
   }
 }
