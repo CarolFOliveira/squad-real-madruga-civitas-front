d3e8d52560ff8e86adc143f65421a399784ed864
felipel7 <felipelseven@gmail.com>
2024-10-18 08:08:48 -0300

    feat(SelectComponent): mostrar erro da validacao

--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 interface IOptions {
@@ -10,10 +10,22 @@ interface IOptions {
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SelectComponent {
   @Input() control!: FormControl;
   @Input() label = '';
   @Input() options: IOptions[] = [];
   @Input() placeholder = '';
+
+  getErrorMessage(): string {
+    const errorMessages = {
+      required: 'Este campo é obrigatório.',
+    };
+
+    for (const [key, message] of Object.entries(errorMessages))
+      if (this.control?.hasError(key)) return message;
+
+    return '';
+  }
 }
