fdbfbabc99375eaab94e4cd7ed0722210140f6bd
felipel7 <felipelseven@gmail.com>
2024-10-23 16:29:14 -0300

    feat(Toolbar): criar estrutura inicial do componente

---
+++ src/app/features/admin/components/toolbar/toolbar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  template: `
+    <mat-toolbar class="toolbar">
+      <app-brand-link />
+      <span class="toolbar__spacer"></span>
+      <button mat-icon-button>
+        <mat-icon>menu</mat-icon>
+      </button>
+    </mat-toolbar>
+  `,
+  styles: [
+    `
+      .toolbar {
+        background: #dbd5e9;
+        height: 65px;
+
+        &__spacer {
+          flex: 1 1 auto;
+        }
+      }
+    `,
+  ],
+})
+export class ToolbarComponent {}
