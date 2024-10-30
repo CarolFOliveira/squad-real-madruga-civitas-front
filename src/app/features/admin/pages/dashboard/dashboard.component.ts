0ce53201b5b2accfa834983d16a781dd3c941355
felipel7 <felipelseven@gmail.com>
2024-10-30 16:10:35 -0300

    feat: adicionar botao para abrir video no dashboard

--- src/app/features/admin/pages/dashboard/dashboard.component.ts
+++ src/app/features/admin/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,24 @@
+// Libs
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+// Components
+import { VideoDialogComponent } from '../../components/video-dialog/video-dialog.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  constructor(public _dialog: MatDialog) {}
+
+  /**
+   * openDialog
+   *
+   * Responsável por abrir o modal do componente `VideoDialogComponent`.
+   */
+  public openDialog(): void {
+    this._dialog.open(VideoDialogComponent);
+  }
+}
