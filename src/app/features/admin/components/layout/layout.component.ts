e4ceed12f5249242de2e56bc5e767961ce1919a8
felipel7 <felipelseven@gmail.com>
2024-10-30 15:50:30 -0300

    style(Layout): adicionar largura maxima na pagina

--- src/app/features/admin/components/layout/layout.component.ts
+++ src/app/features/admin/components/layout/layout.component.ts
@@ -8,6 +8,7 @@ import { LayoutService } from '../../services/layout.service';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit, OnDestroy {
   /**
