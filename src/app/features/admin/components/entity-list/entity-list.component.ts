108540867d4a7028afce5867a946e9207c38f191
felipel7 <felipelseven@gmail.com>
2024-11-05 15:11:03 -0300

    feat: colocar a logica dentro do componente generico

--- src/app/features/admin/components/entity-list/entity-list.component.ts
+++ src/app/features/admin/components/entity-list/entity-list.component.ts
@@ -1,6 +1,12 @@
 // Libs
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+// Services
+import { ActionMenuService } from '../../services/action-menu.service';
+import { EntityService } from '../../services/entity.service';
 
 // Interfaces
 import { IPaginatedItems } from '../../interfaces/IPaginatedItems';
@@ -10,13 +16,76 @@ import { IPaginatedItems } from '../../interfaces/IPaginatedItems';
   templateUrl: './entity-list.component.html',
   styleUrls: ['./entity-list.component.scss'],
 })
-export class EntityListComponent {
+export class EntityListComponent<T> implements OnInit, OnDestroy {
+  private _subscriptions: Subscription = new Subscription();
+  @Input() public endpoint = '';
   @Input() public sectionTitle = '';
-  @Input() public totalItems = 0;
-  @Input() public items: IPaginatedItems[] = [];
-  @Output() public pageChangeEvent = new EventEmitter<PageEvent>();
+  @Input() public mapItem!: (item: T) => IPaginatedItems;
+
+  public paginatedItems: IPaginatedItems[] = [];
+  public totalItems = 0;
+  public currentPage = 1;
+  public isLoading = false;
+
+  constructor(
+    private _entityService: EntityService,
+    private _actionMenuService: ActionMenuService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {}
+
+  public ngOnInit(): void {
+    this.getEntityPage();
+    this.subscribeToEditEvent();
+    this.subscribeToDeleteEvent();
+  }
 
   public onPageChange($event: PageEvent): void {
-    this.pageChangeEvent.emit($event);
+    this.currentPage = $event.pageIndex + 1;
+    this.getEntityPage();
+  }
+
+  private async getEntityPage(): Promise<void> {
+    this.isLoading = true;
+
+    try {
+      const { total, data } = await this._entityService.getEntities<T>({
+        endpoint: this.endpoint,
+        page: this.currentPage,
+      });
+
+      this.totalItems = total;
+      this.paginatedItems = data.map(this.mapItem);
+    } catch (error) {
+      // TODO: mostrar snackbar de erro
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  private subscribeToEditEvent(): void {
+    this._subscriptions.add(
+      this._actionMenuService.editEvent$.subscribe((id: number) => {
+        this._router.navigate([`administrador/editar/${this.endpoint}`, id]);
+      })
+    );
+  }
+
+  private subscribeToDeleteEvent(): void {
+    this._subscriptions.add(
+      this._actionMenuService.deleteEvent$.subscribe(async (id: number) => {
+        try {
+          await this._entityService.deleteEntity(id, this.endpoint);
+          // TODO: mostrar snackbar de sucesso
+        } catch (error) {
+          console.error('Erro:', error);
+          // TODO: mostrar snackbar de erro
+        }
+      })
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this._subscriptions.unsubscribe();
   }
 }
--- src/app/features/admin/pages/student-list/student-list.component.ts
+++ src/app/features/admin/pages/student-list/student-list.component.ts
@@ -1,93 +1,22 @@
 // Libs
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { PageEvent } from '@angular/material/paginator';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 // Interfaces
 import { IPaginatedItems } from '../../interfaces/IPaginatedItems';
 import { IStudent } from '../../interfaces/IStudent';
 
-// Services
-import { ActionMenuService } from '../../services/action-menu.service';
-
-// Services
-import { EntityService } from '../../services/entity.service';
-
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
 })
-export class StudentListComponent implements OnInit, OnDestroy {
-  private readonly endpoint = 'alunos';
-  private _subscriptions: Subscription = new Subscription();
-  public paginatedItems: IPaginatedItems[] = [];
-  public totalItems = 0;
-  public currentPage = 1;
-  public isLoading = false;
+export class StudentListComponent {
+  public readonly endpoint = 'alunos';
 
-  constructor(
-    private _entityService: EntityService,
-    private _actionMenuService: ActionMenuService,
-    private _router: Router
-  ) {}
-
-  public ngOnInit(): void {
-    this.getStudentsPage();
-    this.subscribeToEditEvent();
-    this.subscribeToDeleteEvent();
-  }
-
-  private async getStudentsPage(): Promise<void> {
-    this.isLoading = true;
-
-    try {
-      const { total, data } = await this._entityService.getEntities<IStudent>({
-        endpoint: this.endpoint,
-        page: this.currentPage,
-      });
-
-      this.totalItems = total;
-      this.paginatedItems = data.map((student) => ({
-        id: student.id,
-        title: student.nomeCompleto,
-        subtitle: student.numeroMatricula,
-      }));
-    } catch (error) {
-      // TODO: mostrar snackbar de erro
-    } finally {
-      this.isLoading = false;
-    }
-  }
-
-  public onPageChange($event: PageEvent): void {
-    this.currentPage = $event.pageIndex + 1;
-    this.getStudentsPage();
-  }
-
-  private subscribeToEditEvent(): void {
-    this._subscriptions.add(
-      this._actionMenuService.editEvent$.subscribe((id: number) => {
-        this._router.navigate(['administrador/editar-aluno', id]);
-      })
-    );
-  }
-
-  private subscribeToDeleteEvent(): void {
-    this._subscriptions.add(
-      this._actionMenuService.deleteEvent$.subscribe(async (id: number) => {
-        try {
-          await this._entityService.deleteEntity(id, this.endpoint);
-          // TODO: mostrar snackbar de sucesso
-        } catch (error) {
-          console.error('Erro:', error);
-          // TODO: mostrar snackbar de erro
-        }
-      })
-    );
-  }
-
-  public ngOnDestroy(): void {
-    this._subscriptions.unsubscribe();
+  public mapStudentToPaginatedItems(student: IStudent): IPaginatedItems {
+    return {
+      id: student.id,
+      title: student.name,
+      subtitle: student.enrollmentNumber,
+    };
   }
 }
