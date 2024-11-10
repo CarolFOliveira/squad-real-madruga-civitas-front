a353f9eb47897bf0286be08666906d866dcd68ad
felipel7 <felipelseven@gmail.com>
2024-11-10 20:10:18 -0300

    feat(TeacherCreate): gerar componente

---
+++ src/app/features/admin/pages/teacher-create/teacher-create.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-create',
+  templateUrl: './teacher-create.component.html',
+  styleUrls: ['./teacher-create.component.scss'],
+})
+export class TeacherCreateComponent {}
