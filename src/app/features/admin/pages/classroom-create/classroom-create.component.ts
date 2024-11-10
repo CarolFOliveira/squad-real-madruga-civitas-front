8294828051f32f1543ad40b43d9bc944c825f088
felipel7 <felipelseven@gmail.com>
2024-11-10 20:09:40 -0300

    feat(ClassRoom): gerar componente

---
+++ src/app/features/admin/pages/classroom-create/classroom-create.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classroom-create',
+  templateUrl: './classroom-create.component.html',
+  styleUrls: ['./classroom-create.component.scss'],
+})
+export class ClassroomCreateComponent {}
