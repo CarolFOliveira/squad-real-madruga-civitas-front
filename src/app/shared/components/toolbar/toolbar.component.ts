5319df6e2d6664eaf7842047a17726c0ede29657
felipel7 <felipelseven@gmail.com>
2024-11-18 07:59:41 -0300

    refactor: alterar implementacao da toolbar

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,18 +1,13 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 /**
  * ToolbarComponent
  *
- * Componente que representa uma navbar que exibe um botão de menu e uma logo.
+ * Componente que representa uma navbar que exibe uma logo no lado esquerdo.
  */
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {
-  /**
-   * Emite um evento chamado `menuClick` quando o botão de menu é clicado.
-   */
-  @Output() menuClick = new EventEmitter<void>();
-}
+export class ToolbarComponent {}
