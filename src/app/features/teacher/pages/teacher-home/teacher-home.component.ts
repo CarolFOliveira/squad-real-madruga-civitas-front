6ac12ad336525fa19995a2beee10ad4f480cee14
felipel7 <felipelseven@gmail.com>
2024-11-19 12:34:26 -0300

    feat: adicionar filtros para pesquisa e ordenacao da tabela

--- src/app/features/teacher/pages/teacher-home/teacher-home.component.ts
+++ src/app/features/teacher/pages/teacher-home/teacher-home.component.ts
@@ -1,62 +1,56 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatSelectChange } from '@angular/material/select';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-interface IUserData {
-  link: string;
+/**
+ * Interface com os dados do Aluno que serão exibidos na tabela.
+ */
+interface IUserTableData {
+  id: number;
   name: string;
   performance: string;
 }
 
+/**
+ * Interface com os dados dos cards exibidos na interface.
+ */
 interface ICardData {
   icon: string;
   description: string;
   descriptionValue: number;
 }
 
+/**
+ * TeacherHomeComponent
+ *
+ * Componente que representa a página inicial do professor.
+ */
 @Component({
   selector: 'app-teacher-home',
   templateUrl: './teacher-home.component.html',
   styleUrls: ['./teacher-home.component.scss'],
 })
-export class TeacherHomeComponent {
+export class TeacherHomeComponent implements AfterViewInit {
+  @ViewChild(MatSort) sort!: MatSort;
+
+  /**
+   * Array de `string` que representa as colunas exibidas na tabela.
+   */
   public displayedColumns: string[] = ['name', 'performance', 'link'];
-  public dataSource: MatTableDataSource<IUserData>;
 
-  constructor() {
-    this.dataSource = new MatTableDataSource(this.users);
-  }
+  /**
+   * Array de dados do tipo `MatTableDataSource` que permite funcionalidades nativas como filtros e paginação.
+   */
+  public dataSource: MatTableDataSource<IUserTableData>;
 
-  // TODO: excluir exemplo
-  options = [
-    { value: 1, viewValue: '3º ano A' },
-    { value: 2, viewValue: '3º ano B' },
-    { value: 3, viewValue: '3º ano C' },
-    { value: 4, viewValue: '3º ano D' },
-  ];
-
-  // TODO: excluir dados de exemplo
-  public users: IUserData[] = [
-    {
-      name: 'Fulano',
-      performance: 'bom',
-      link: '123',
-    },
-    {
-      name: 'Fulano 2',
-      performance: 'normal',
-      link: '123',
-    },
-    {
-      name: 'Fulano 3',
-      performance: 'ruim',
-      link: '123',
-    },
-  ];
-
+  /**
+   * Array de cards que exibem informações resumidas da aplicação para o usuário.
+   */
   public cards: ICardData[] = [
     {
       description: 'Número de Turmas',
+      // TODO: Buscar valores do backend
       descriptionValue: 6,
       icon: 'school',
     },
@@ -72,16 +66,98 @@ export class TeacherHomeComponent {
     },
   ];
 
-  public getPerformance(value: string): string {
+  /**
+   * Termo de busca utilizado para filtrar os dados da tabela.
+   */
+  public term = '';
+
+  constructor() {
+    this.dataSource = new MatTableDataSource(this.users);
+    this.dataSource.sort = this.sort;
+  }
+
+  /**
+   * ngAfterViewInit
+   *
+   * Método do do Angular que é chamado após a inicialização da view para configurar o sort da tabela.
+   */
+  public ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  // TODO: Buscar turmas do professor no backend
+  public options = [
+    { value: 1, viewValue: '3º ano A' },
+    { value: 2, viewValue: '3º ano B' },
+    { value: 3, viewValue: '3º ano C' },
+    { value: 4, viewValue: '3º ano D' },
+  ];
+
+  // TODO: Buscar alunos de uma determinada turma no backend
+  public users: IUserTableData[] = [
+    {
+      id: 1,
+      name: 'Bianca Souza',
+      performance: 'bom',
+    },
+    {
+      id: 2,
+      name: 'Carlos Silva',
+      performance: 'normal',
+    },
+    {
+      id: 3,
+      name: 'Daniel Silva',
+      performance: 'ruim',
+    },
+  ];
+
+  /**
+   * getPerformance
+   *
+   * Retorna a classe CSS correspondente ao desempenho fornecido.
+   *
+   * @param value Uma `string` que corresponde ao valor de desempenho vindo do backend.
+   * @returns Uma `string` que representa a tradução do desempenho em inglês.
+   */
+  public getPerformanceClass(value: string): string {
     const performance: { [key: string]: string } = {
       bom: 'good',
-      normal: 'satisfactory',
+      normal: 'normal',
       ruim: 'bad',
     };
+
     return performance[value];
   }
 
+  /**
+   * onSelect
+   *
+   * Captura a opção escolhida pelo usuário na interface.
+   *
+   * @param $event Evento de mudança gerado pelo `MatSelectChange`, contendo o valor selecionado.
+   */
   public onSelect($event: MatSelectChange): void {
+    // TODO: chamar o backend com o valor selecionado
     console.log({ value: $event.value });
+  }
+
+  /**
+   * applyFilter
+   *
+   * Aplica o filtro na tabela utilizando o termo de pesquisa `term`.
+   */
+  public applyFilter() {
+    this.dataSource.filter = this.term.trim().toLowerCase();
+  }
+
+  /**
+   * clearSearch
+   *
+   * Reinicia o termo de pesquisa `term` e limpa os filtros da tabela.
+   */
+  public clearSearch(): void {
+    this.term = '';
+    this.applyFilter();
   }
 }
--- src/app/features/teacher/teacher.module.ts
+++ src/app/features/teacher/teacher.module.ts
@@ -1,6 +1,7 @@
 // Libs
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 // App Modules
 import { SharedModule } from '../../shared/shared.module';
@@ -11,7 +12,9 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
@@ -28,8 +31,11 @@ const MODULES = [
   MatFormFieldModule,
   MatIconModule,
   MatSelectModule,
+  MatSortModule,
   MatTableModule,
+  MatInputModule,
   MatTooltipModule,
+  FormsModule,
   SharedModule,
   TeacherRoutingModule,
 ];
