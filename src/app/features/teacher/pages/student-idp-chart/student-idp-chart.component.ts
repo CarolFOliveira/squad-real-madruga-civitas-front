dd894563b6f3ebe135ef8bd024db42fbe281e441
felipel7 <felipelseven@gmail.com>
2024-11-24 22:09:58 -0300

    refactor(StudentIDPChart): alterar implementacao da pagina

--- src/app/features/teacher/pages/student-idp-chart/student-idp-chart.component.ts
+++ src/app/features/teacher/pages/student-idp-chart/student-idp-chart.component.ts
@@ -1,61 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ApexOptions } from 'ng-apexcharts';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-student-idp-chart',
   templateUrl: './student-idp-chart.component.html',
   styleUrls: ['./student-idp-chart.component.scss'],
 })
-export class StudentIdpChartComponent implements OnInit {
-  public chartOptions!: Partial<ApexOptions>;
-
-  public ngOnInit(): void {
-    this.chartOptions = {
-      series: [
-        {
-          name: 'Mês anterior',
-          data: [2, 3, 2],
-          color: '#65558F',
-        },
-        {
-          name: 'Mês atual',
-          data: [4, 3, 4],
-          color: '#D0BCFF',
-        },
-      ],
-      chart: {
-        type: 'bar',
-        height: 350,
-
-        toolbar: {
-          show: false,
-        },
-        animations: {
-          speed: 400,
-        },
-      },
-
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          columnWidth: '50%',
-          borderRadius: 6,
-        },
-      },
-      colors: ['#FF5733', '#33B5E5'],
-      dataLabels: {
-        enabled: false,
-      },
-      xaxis: {
-        categories: [
-          'Desenvolvimento acadêmico',
-          'Inteligência emocional',
-          'Responsabilidade',
-        ],
-      },
-      yaxis: {
-        max: 5,
-      },
-    };
-  }
-}
+export class StudentIdpChartComponent {}
