358eaa9612c66c6e2a507124317cbeee6162b9a4
felipel7 <felipelseven@gmail.com>
2024-11-26 11:24:24 -0300

    feat(StudentOverview): carregar dados do componente dinamicamente

--- src/app/features/teacher/components/student-overview/student-overview.component.ts
+++ src/app/features/teacher/components/student-overview/student-overview.component.ts
@@ -1,10 +1,27 @@
+//Libs
 import { Component, Input } from '@angular/core';
 
+// Interfaces
+import { IStudentIDPSummary } from '../../interfaces/IStudentIDPSummary';
+
+/**
+ * StudentOverviewComponent
+ *
+ * Componente responsável por exibir informações básicas sobre o aluno, professor e o PDI.
+ */
 @Component({
   selector: 'app-student-overview',
   templateUrl: './student-overview.component.html',
   styleUrls: ['./student-overview.component.scss'],
 })
 export class StudentOverviewComponent {
+  /**
+   * Título da seção exibida no componente.
+   */
   @Input() public title = '';
+
+  /**
+   * Informações do aluno que serão exibidas em tela.
+   */
+  @Input() public student!: IStudentIDPSummary;
 }
---
+++ src/app/features/teacher/interfaces/IStudentIDPSummary.ts
@@ -0,0 +1,10 @@
+/**
+ * Interface com as informações resumidas sobre o PDI do aluno.
+ */
+export interface IStudentIDPSummary {
+  classroomName: string;
+  studentName: string;
+  teacherName: string;
+  enrollmentNumber: string;
+  idpRegisterDate: string;
+}
