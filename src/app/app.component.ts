0b34251961d19a3008213b1a7802d82476d4f377
felipel7 <felipelseven@gmail.com>
2024-12-01 15:36:32 -0300

    style(App): remover codigo nao utilizado no componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,7 @@
-//Libs
 import { Component } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-
-// Services
-import { AuthService } from './features/auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  /**
-   * Indica se o usuário está autenticado ou não.
-   */
-  public isAuthenticated$: BehaviorSubject<boolean>;
-
-  constructor(private _authService: AuthService) {
-    this.isAuthenticated$ = this._authService.isAuthenticated$;
-  }
-}
+export class AppComponent {}
